cb63625118538dd670355ea43206e003145bf3c9
David Ogbeide <boyowao@gmail.com>
2018-05-20 15:06:02 -0400

    Configure production webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,37 @@
 const path = require('path');
 
+
 const PUBLIC_PATH = path.join(__dirname, 'public');
 
-module.exports = {
-    entry: './src/app.js',
-    mode: 'development',
-    output: {
-        path: PUBLIC_PATH,
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.jsx?$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: PUBLIC_PATH,
-        historyApiFallback: true
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    return {
+        entry: './src/app.js',
+        mode: 'development',
+        output: {
+            path: PUBLIC_PATH,
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.jsx?$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: PUBLIC_PATH,
+            historyApiFallback: true
+        }
     }
 };
